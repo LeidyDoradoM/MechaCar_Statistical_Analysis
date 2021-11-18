f3d80c916384bd309d25a421b17593cadbd53734
Leidy Dorado <doradoleidypao@gmail.com>
2021-11-18 16:13:06 -0500

    update R script/deliv1

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,7 +3,7 @@ library(dplyr) # for %>%
 MechaCar_df <- read.csv(file='Resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
 #Multiple linear regression model
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_df) 
+multi_lm = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_df) 
 #generate summary statistics
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_df) )
+summary(multi_lm )
 
