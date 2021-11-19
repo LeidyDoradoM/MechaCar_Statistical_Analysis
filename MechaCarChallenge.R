e74e83b8dade6a604394229d88a7c67fbbb95f1a
Leidy Dorado <doradoleidypao@gmail.com>
2021-11-18 22:19:03 -0500

    add R script for deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,9 +1,19 @@
 library(dplyr) # for %>%
+library("tidyverse")
 
-MechaCar_df <- read.csv(file='Resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-
+## ---- Deliverable 1 -------- ##
+# read csv file
+MechaCar <- read.csv(file='Resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 #Multiple linear regression model
-multi_lm = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_df) 
+multi_lm = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar) 
 #generate summary statistics
 summary(multi_lm )
 
+## ------ Deliverable 2 -------##
+# read csv file
+suspCoil <- read.csv(file='Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors=F)
+# create the summary dataframe
+total_summary <- suspCoil %>% summarise(mean = mean(PSI), median = median(PSI), variance = var(PSI) , SD = sd(PSI))
+#---grouping by manufacturing lot and summarising
+lot_summary <- suspCoil %>% group_by(Manufacturing_Lot) %>% 
+  summarise(mean = mean(PSI), median = median(PSI), variance = var(PSI) , SD = sd(PSI))
